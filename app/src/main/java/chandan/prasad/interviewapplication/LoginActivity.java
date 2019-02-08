4cd31ef253ab56f18e656101ac01bc0848858dcb
Tata Strive <snehal.jadhav450@gmail.com>
2019-02-08 17:20:15 +0530

    Initial commit

---
+++ app/src/main/java/chandan/prasad/interviewapplication/LoginActivity.java
@@ -0,0 +1,36 @@
+package chandan.prasad.interviewapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView signupText;
+    EditText emailEdittext, passwordEdittext;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        signupText = findViewById(R.id.signup);
+        emailEdittext = findViewById(R.id.email);
+        passwordEdittext = findViewById(R.id.password);
+        loginButton = findViewById(R.id.signin);
+
+    }
+    public void loginMe(View view){
+        Intent mainAct = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(mainAct);
+    }
+
+    public void newUser(View view) {
+        Intent reg = new Intent(getApplicationContext(),RegisterActivity.class);
+        startActivity(reg);
+    }
+
+}
